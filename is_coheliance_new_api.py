77d569f3c0557f3879efdfc4b27eda60f79cb6bd
Tony Galmiche <tony.galmiche@infosaone.com>
2017-03-08 14:53:29 +0100

    taux_km modifiable dans fiche de frais

--- is_coheliance_new_api.py
+++ is_coheliance_new_api.py
@@ -6,7 +6,8 @@ from openerp import pooler
 from openerp import models,fields,api
 from openerp.tools.translate import _
 
-from datetime import datetime, timedelta
+#from datetime import datetime, timedelta
+import datetime
 
 
 #TODO, j'ai voulu rendre modifiable le compte des lignes des factures validées et payées, mais ce n'est pas possible
@@ -14,6 +15,13 @@ from datetime import datetime, timedelta
 #    _inherit = 'account.invoice.line'
 #    
 #    is_number = fields.Char('N°Facture', related='invoice_id.number', readonly=True)
+
+
+def _date_creation():
+    now  = datetime.date.today()
+    #print "now=",datetime.datetime.now().strftime('%H:%M:%S')
+    return now.strftime('%Y-%m-%d')
+
 
 
 class is_frais(models.Model):
@@ -28,19 +36,26 @@ class is_frais(models.Model):
     taux_km        = fields.Float(u"Taux indemnité kilométrique")
     ligne_ids      = fields.One2many('is.frais.ligne', 'frais_id', u'Lignes')
 
+
+    def _taux_km(self,cr):
+        sql="select taux_km from res_company limit 1";
+        cr.execute(sql)
+        taux_km=0
+        for row in cr.fetchall():
+            taux_km=row[0]
+        return taux_km
+
+
     _defaults = {
         'name': '',
         'intervenant_id': lambda obj, cr, uid, context: uid,
-        'date_creation': datetime.today().strftime('%Y-%m-%d'),
+        'date_creation': lambda *a: _date_creation(),
+        'taux_km': lambda self, cr, uid, context: self._taux_km(cr),
     }
 
 
     @api.model
     def create(self, vals):
-        company_obj = self.env['res.company']
-        taux_km = company_obj.browse(1).taux_km
-        vals["taux_km"]=taux_km
-
         data_obj = self.env['ir.model.data']
         sequence_ids = data_obj.search([('name','=','is_frais_seq')])
         if sequence_ids:
@@ -48,6 +63,19 @@ class is_frais(models.Model):
             vals['name'] = self.env['ir.sequence'].get_id(sequence_id, 'id')
         res = super(is_frais, self).create(vals)
         return res
+
+
+    @api.multi
+    def write(self, vals):
+        for obj in self:
+            if 'taux_km' in vals:
+                taux_km=vals['taux_km']
+                for lig in obj.ligne_ids:
+                    if lig.km:
+                        lig.montant_ht=lig.km*taux_km
+        res = super(is_frais, self).write(vals)
+        return res
+
 
 
 
@@ -80,7 +108,7 @@ class is_frais_ligne(models.Model):
     refacture      = fields.Boolean(u"Frais refacturé au client")
 
     _defaults = {
-        'date': datetime.today().strftime('%Y-%m-%d'),
+        'date': lambda *a: _date_creation(),
         'refacturable': 'oui',
         'refacture': False,
     }
@@ -88,21 +116,20 @@ class is_frais_ligne(models.Model):
 
     @api.model
     def create(self,vals):
+        obj = super(is_frais_ligne, self).create(vals)
         if 'km' in vals:
             if vals['km']!=0:
-                company_obj = self.env['res.company']
-                taux_km = company_obj.browse(1).taux_km
-                vals['montant_ht']=vals['km']*taux_km
-        res = super(is_frais_ligne, self).create(vals)
-        return res
+                taux_km=obj.frais_id.taux_km
+                obj.montant_ht=vals['km']*taux_km
+        return obj
 
 
     @api.multi
     def write(self, vals):
-        if 'km' in vals:
-            company_obj = self.env['res.company']
-            taux_km = company_obj.browse(1).taux_km
-            vals['montant_ht']=vals['km']*taux_km
+        for obj in self:
+            if 'km' in vals:
+                taux_km=obj.frais_id.taux_km
+                vals['montant_ht']=vals['km']*taux_km
         res = super(is_frais_ligne, self).write(vals)
         return res
 
