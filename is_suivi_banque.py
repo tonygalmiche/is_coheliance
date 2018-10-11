c52a8658bfee198d3143157b316360573230db60
Tony Galmiche <tony.galmiche@infosaone.com>
2018-10-11 15:04:23 +0200

    add banque import société générale

--- is_suivi_banque.py
+++ is_suivi_banque.py
@@ -5,11 +5,15 @@ import datetime
 import codecs
 import unicodedata
 import base64
+import csv, cStringIO
 
 
 def _date_creation():
     now  = datetime.date.today()
     return now.strftime('%Y-%m-%d')
+
+
+
 
 
 class IsSuiviBanque(models.Model):
@@ -41,82 +45,145 @@ class IsImportBanque(models.Model):
     _order='name desc'
 
     name               = fields.Date(u"Date de création", default=lambda *a: _date_creation())
+    banque = fields.Selection([
+        ('BP', 'Banque Populaire'),
+        ('SG', 'Société Générale'),
+    ], 'Banque')
     file_operation_ids = fields.Many2many('ir.attachment', 'is_import_banque_operation_attachment_rel', 'doc_id', 'file_id', 'Opérations à importer')
     file_cb_ids        = fields.Many2many('ir.attachment', 'is_import_banque_cb_attachment_rel'       , 'doc_id', 'file_id', 'Détail CB à importer')
     ligne_ids          = fields.One2many('is.suivi.banque', 'import_banque_id', u'Lignes')
 
 
+
+    @api.multi
+    def _ecrire_ligne(self,ligne,date,libelle,debit,credit):
+        for obj in self:
+            vals={
+                'import_banque_id'  : obj.id,
+                'ligne'             : (ligne-1),
+                'date'              : date,
+                'libelle'           : libelle,
+                'debit'             : debit,
+                'credit'            : credit,
+            }
+            self.env['is.suivi.banque'].create(vals)
+
+
+
     @api.multi
     def action_importer_fichier(self):
         for obj in self:
-            ct=0
-            obj.ligne_ids.unlink()
-            for attachment in obj.file_operation_ids:
-                attachment=base64.decodestring(attachment.datas)
-                attachment=attachment.decode('iso-8859-1').encode('utf8')
-                csvfile=attachment.split("\n")
-                tab=[]
-                for row in csvfile:
-                    ct=ct+1
-                    if ct>1:
-                        lig=row.split(";")
-                        if len(lig)>5:
-                            date    = lig[1]
-                            libelle = lig[3]
-                            montant = lig[6].replace(',', '.')
-                            try:
-                                montant = float(montant)
-                            except ValueError:
-                                montant=0
-                            debit=0
-                            credit=0
-                            if montant<0:
-                                debit=-montant
-                            else:
-                                credit=montant
-                            vals={
-                                'import_banque_id'  : obj.id,
-                                'ligne'             : (ct-1),
-                                'date'              : date,
-                                'libelle'           : libelle,
-                                'debit'             : debit,
-                                'credit'            : credit,
-                            }
-                            self.env['is.suivi.banque'].create(vals)
-            for attachment in obj.file_cb_ids:
-                attachment=base64.decodestring(attachment.datas)
-                attachment=attachment.decode('iso-8859-1').encode('utf8')
-                csvfile=attachment.split("\n")
-                tab=[]
-                for row in csvfile:
-                    ct=ct+1
-                    if ct>1:
-                        lig=row.split(";")
-                        if len(lig)>5:
-                            date    = lig[1]
-                            libelle = lig[2]
-                            ref_cb  = lig[3]
-                            montant = lig[4].replace(',', '.')
-                            try:
-                                montant = float(montant)
-                            except ValueError:
-                                montant=0
-                            debit=0
-                            credit=0
-                            if montant<0:
-                                debit=-montant
-                            else:
-                                credit=montant
-                            vals={
-                                'import_banque_id'  : obj.id,
-                                'ligne'             : (ct-1),
-                                'date'              : date,
-                                'libelle'           : libelle,
-                                'ref_cb'            : ref_cb,
-                                'debit'             : debit,
-                                'credit'            : credit,
-                            }
-                            self.env['is.suivi.banque'].create(vals)
+            if obj.banque=='BP':
+                ct=0
+                obj.ligne_ids.unlink()
+                for attachment in obj.file_operation_ids:
+                    attachment=base64.decodestring(attachment.datas)
+                    attachment=attachment.decode('iso-8859-1').encode('utf8')
+                    csvfile=attachment.split("\n")
+                    tab=[]
+                    for row in csvfile:
+                        ct=ct+1
+                        if ct>1:
+                            lig=row.split(";")
+                            if len(lig)>5:
+                                date    = lig[1]
+                                libelle = lig[3]
+                                montant = lig[6].replace(',', '.')
+                                try:
+                                    montant = float(montant)
+                                except ValueError:
+                                    montant=0
+                                debit=0
+                                credit=0
+                                if montant<0:
+                                    debit=-montant
+                                else:
+                                    credit=montant
+                                vals={
+                                    'import_banque_id'  : obj.id,
+                                    'ligne'             : (ct-1),
+                                    'date'              : date,
+                                    'libelle'           : libelle,
+                                    'debit'             : debit,
+                                    'credit'            : credit,
+                                }
+                                self.env['is.suivi.banque'].create(vals)
+                for attachment in obj.file_cb_ids:
+                    attachment=base64.decodestring(attachment.datas)
+                    attachment=attachment.decode('iso-8859-1').encode('utf8')
+                    csvfile=attachment.split("\n")
+                    tab=[]
+                    for row in csvfile:
+                        ct=ct+1
+                        if ct>1:
+                            lig=row.split(";")
+                            if len(lig)>5:
+                                date    = lig[1]
+                                libelle = lig[2]
+                                ref_cb  = lig[3]
+                                montant = lig[4].replace(',', '.')
+                                try:
+                                    montant = float(montant)
+                                except ValueError:
+                                    montant=0
+                                debit=0
+                                credit=0
+                                if montant<0:
+                                    debit=-montant
+                                else:
+                                    credit=montant
+                                vals={
+                                    'import_banque_id'  : obj.id,
+                                    'ligne'             : (ct-1),
+                                    'date'              : date,
+                                    'libelle'           : libelle,
+                                    'ref_cb'            : ref_cb,
+                                    'debit'             : debit,
+                                    'credit'            : credit,
+                                }
+                                self.env['is.suivi.banque'].create(vals)
+
+            if obj.banque=='SG':
+                ct=0
+                obj.ligne_ids.unlink()
+                for attachment in obj.file_operation_ids:
+                    attachment=base64.decodestring(attachment.datas)
+                    attachment=attachment.decode('iso-8859-1').encode('utf8')
+                    csvfile=attachment.split("\n")
+                    csvfile = csv.reader(csvfile, delimiter=';')
+                    date=''
+                    libelle=''
+                    debit=0
+                    credit=0
+                    ligne=0
+                    for ct, lig in enumerate(csvfile):
+                        if len(lig)>=7:
+                            ct=ct+1
+                            if ct>1:
+                                if lig[0]:
+                                    if date:
+                                        self._ecrire_ligne(ligne,date,libelle,debit,credit)
+                                    ligne=ligne+1
+                                    date    = lig[0]
+                                    try:
+                                        d = datetime.datetime.strptime(date, '%d/%m/%Y')
+                                        date = d.strftime('%Y-%m-%d')
+                                    except ValueError:
+                                        date=False
+                                    libelle = lig[1]
+                                    debit   = lig[2].replace(',', '.').replace(' ', '')
+                                    try:
+                                        debit = -float(debit)
+                                    except ValueError:
+                                        debit=0
+                                    credit  = lig[3].replace(',', '.').replace(' ', '')
+                                    try:
+                                        credit = float(credit)
+                                    except ValueError:
+                                        credit=0
+                                else:
+                                    libelle=libelle+' '+lig[1]
+                    self._ecrire_ligne(ligne,date,libelle,debit,credit)
             return {
                 'name': u'Suivi banque '+obj.name,
                 'view_mode': 'tree,form',
@@ -129,6 +196,7 @@ class IsImportBanque(models.Model):
                     'default_import_banque_id': obj.id,
                 },
                 'type': 'ir.actions.act_window',
+                'limit': 500,
             }
 
 
