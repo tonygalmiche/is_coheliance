1e9a00d7fdecbf59b0be7fc20fb5e7e6451e6f55
Tony Galmiche <tony.galmiche@infosaone.com>
2020-01-22 14:54:40 +0100

    add affaire

--- is_export_compta.py
+++ is_export_compta.py
@@ -10,7 +10,7 @@ class is_export_compta(models.Model):
     _order='name desc'
 
     name               = fields.Char("N°Folio"      , readonly=True)
-    type_interface     = fields.Selection([('ventes', u'Ventes'),('achats', u'Achats')], "Interface", required=True)
+    type_interface     = fields.Selection([('ventes', u'Ventes'),('achats', u'Achats'),('banque', u'Banque')], "Interface", required=True)
     date_debut         = fields.Date("Date de début")
     date_fin           = fields.Date("Date de fin")
     num_debut          = fields.Char("N° facture début")
@@ -72,6 +72,8 @@ class is_export_compta(models.Model):
                         ai.type, 
                         rp.is_code_fournisseur,
                         ai.is_nom_fournisseur,
+                        aml.account_id,
+                        ai.is_affaire_id,
                         sum(aml.debit), 
                         sum(aml.credit)
 
@@ -79,11 +81,27 @@ class is_export_compta(models.Model):
                                                inner join account_account aa             on aml.account_id=aa.id
                                                inner join res_partner rp                 on ai.partner_id=rp.id
                     WHERE ai.id="""+str(invoice.id)+"""
-                    GROUP BY ai.date_invoice, ai.number, rp.name, aml.name, aa.code, ai.type, ai.date_due, rp.supplier,rp.is_code_fournisseur,ai.is_nom_fournisseur
-                    ORDER BY ai.date_invoice, ai.number, rp.name, aml.name, aa.code, ai.type, ai.date_due, rp.supplier,rp.is_code_fournisseur,ai.is_nom_fournisseur
+                    GROUP BY ai.date_invoice, ai.number, rp.name, aml.name, aa.code, ai.type, ai.date_due, rp.supplier,rp.is_code_fournisseur,ai.is_nom_fournisseur,aml.account_id,ai.is_affaire_id
+                    ORDER BY ai.date_invoice, ai.number, rp.name, aml.name, aa.code, ai.type, ai.date_due, rp.supplier,rp.is_code_fournisseur,ai.is_nom_fournisseur,aml.account_id,ai.is_affaire_id
                 """
                 cr.execute(sql)
                 for row in cr.fetchall():
+
+                    #** Recherche de l'affaire *********************************
+                    affaire    = ''
+                    affaire_id = row[9] or False
+                    filter=[
+                        ('account_id', '=', row[8]),
+                        ('name'      , '=', row[4]),
+                    ]
+                    lines = self.env['account.invoice.line'].search(filter)
+                    for line in lines:
+                        if line.is_affaire_id.id:
+                            affaire_id = line.is_affaire_id.id
+                    if affaire_id : 
+                        affaire = self.env['is.affaire'].browse(affaire_id).name
+                    #***********************************************************
+
                     nom_fournisseur=row[7]
                     if nom_fournisseur==None:
                         nom_fournisseur=row[3]
@@ -97,8 +115,9 @@ class is_export_compta(models.Model):
                         'journal'           : journal,
                         'compte'            : compte,
                         'libelle'           : libelle,
-                        'debit'             : row[8],
-                        'credit'            : row[9],
+                        'affaire'           : affaire,
+                        'debit'             : row[10],
+                        'credit'            : row[11],
                         'devise'            : 'E',
                         'piece'             : row[2],
                         'commentaire'       : False,
@@ -117,6 +136,7 @@ class is_export_compta_ligne(models.Model):
     compte           = fields.Char("N°Compte")
     piece            = fields.Char("Pièce")
     libelle          = fields.Char("Libellé")
+    affaire          = fields.Char('Affaire')
     debit            = fields.Float("Débit")
     credit           = fields.Float("Crédit")
     devise           = fields.Char("Devise")
